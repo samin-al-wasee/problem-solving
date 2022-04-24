59ef08629a30ab7eb018181339f62fcc9d27bfd7
17r4f33d <69575470+17r4f33d@users.noreply.github.com>
2022-04-24 12:28:44 +0600

    Add files via upload

---
+++ even-odd-fibonacci-sum.py
@@ -0,0 +1,20 @@
+# Given a range (inclusive), find out the sum of all the even and odd fibonacci numbers in that range seperately.
+from time import time
+start = time()
+def even_odd_fibonacci(start_inclusive, end_inclusive):
+    prev_prev = 0
+    prev = 1
+    even_sum = 0
+    odd_sum = 0
+    while prev <= end_inclusive:
+        if prev >= start_inclusive:
+            if prev % 2 == 0:
+                even_sum += prev
+            else:
+                odd_sum += prev
+        nextt = prev_prev + prev
+        prev_prev = prev
+        prev = nextt
+    return even_sum, odd_sum
+print(even_odd_fibonacci(0, 20))
+print("Took {taken_time} seconds".format(taken_time = time() - start))
